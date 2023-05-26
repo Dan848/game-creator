6ae7a7c1edd5d577626bb9e9efa5817da50e3cfa
G-Lumia <giorgiolumia@gmail.com>
2023-05-26 16:59:22 +0200

    db_game created and characters populated

---
+++ config/db_game.php
@@ -0,0 +1,176 @@
+<?php
+return $game = [
+    'characters' => [
+        [
+            'name' => 'Arthur',
+            'description' => 'A brave knight with a strong sense of justice.',
+            'attack' => 10,
+            'defence' => 5,
+            'speed' => 8,
+            'intelligence' => 7,
+            'life' => 100
+        ],
+        [
+            'name' => 'Elena',
+            'description' => 'A skilled archer with a keen eye and deadly accuracy.',
+            'attack' => 8,
+            'defence' => 6,
+            'speed' => 9,
+            'intelligence' => 6,
+            'life' => 90
+        ],
+        [
+            'name' => 'Gareth',
+            'description' => 'A mighty warrior who wields a massive two-handed sword.',
+            'attack' => 12,
+            'defence' => 9,
+            'speed' => 5,
+            'intelligence' => 4,
+            'life' => 110
+        ],
+        [
+            'name' => 'Sylvia',
+            'description' => 'A mysterious sorceress with unparalleled magical abilities.',
+            'attack' => 6,
+            'defence' => 4,
+            'speed' => 7,
+            'intelligence' => 10,
+            'life' => 80
+        ],
+        [
+            'name' => 'Liam',
+            'description' => 'A quick and agile rogue skilled in stealth and thievery.',
+            'attack' => 7,
+            'defence' => 3,
+            'speed' => 12,
+            'intelligence' => 5,
+            'life' => 95
+        ],
+        [
+            'name' => 'Isabella',
+            'description' => 'A master of the arcane arts, capable of summoning powerful spells.',
+            'attack' => 9,
+            'defence' => 7,
+            'speed' => 6,
+            'intelligence' => 11,
+            'life' => 85
+        ],
+        [
+            'name' => 'Oscar',
+            'description' => 'A seasoned warrior with exceptional combat skills and battle experience.',
+            'attack' => 11,
+            'defence' => 8,
+            'speed' => 6,
+            'intelligence' => 5,
+            'life' => 105
+        ],
+        [
+            'name' => 'Aria',
+            'description' => 'A gifted healer who can mend wounds and restore vitality.',
+            'attack' => 4,
+            'defence' => 3,
+            'speed' => 5,
+            'intelligence' => 12,
+            'life' => 75
+        ],
+        [
+            'name' => 'Victor',
+            'description' => 'A fearless gladiator skilled in combat and known for his bravery.',
+            'attack' => 8,
+            'defence' => 10,
+            'speed' => 7,
+            'intelligence' => 6,
+            'life' => 95
+        ],
+        [
+            'name' => 'Luna',
+            'description' => 'A mysterious sorceress who can control the elements and wield powerful magic.',
+            'attack' => 9,
+            'defence' => 5,
+            'speed' => 7,
+            'intelligence' => 9,
+            'life' => 90
+        ],
+        [
+            'name' => 'Xander',
+            'description' => 'A skilled rogue with quick reflexes and unparalleled agility.',
+            'attack' => 7,
+            'defence' => 4,
+            'speed' => 11,
+            'intelligence' => 6,
+            'life' => 85
+        ],
+        [
+            'name' => 'Sophia',
+            'description' => 'A wise and powerful sorceress who commands the forces of nature.',
+            'attack' => 6,
+            'defence' => 5,
+            'speed' => 6,
+            'intelligence' => 12,
+            'life' => 80
+        ],
+        [
+            'name' => 'Maximus',
+            'description' => 'A fearless warrior with unmatched strength and combat skills.',
+            'attack' => 12,
+            'defence' => 9,
+            'speed' => 6,
+            'intelligence' => 4,
+            'life' => 110
+        ],
+        [
+            'name' => 'Felix',
+            'description' => 'A master swordsman who can strike with precision and lethal speed.',
+            'attack' => 10,
+            'defence' => 6,
+            'speed' => 8,
+            'intelligence' => 5,
+            'life' => 100
+        ],
+        [
+            'name' => 'Aurora',
+            'description' => 'A radiant sorceress with the power to manipulate light and harness its energy.',
+            'attack' => 7,
+            'defence' => 4,
+            'speed' => 7,
+            'intelligence' => 11,
+            'life' => 80
+        ],
+        [
+            'name' => 'Leo',
+            'description' => 'A fierce warrior who can channel the energy of the sun into devastating attacks.',
+            'attack' => 11,
+            'defence' => 8,
+            'speed' => 5,
+            'intelligence' => 6,
+            'life' => 105
+        ],
+        [
+            'name' => 'Nova',
+            'description' => 'A gifted mage with control over the elements and the ability to summon celestial beings.',
+            'attack' => 6,
+            'defence' => 5,
+            'speed' => 6,
+            'intelligence' => 12,
+            'life' => 75
+        ],
+        [
+            'name' => 'Cassandra',
+            'description' => 'A skilled assassin who strikes from the shadows with deadly precision.',
+            'attack' => 9,
+            'defence' => 4,
+            'speed' => 10,
+            'intelligence' => 8,
+            'life' => 95
+        ],
+        [
+            'name' => 'Valeria',
+            'description' => 'A fierce and relentless warrior who can endure any hardship and overcome any obstacle.',
+            'attack' => 10,
+            'defence' => 10,
+            'speed' => 5,
+            'intelligence' => 6,
+            'life' => 100
+        ]
+    ]
+];
