be29c633b4d53982a5844c85e0040d3126e47019
NelloRossi <rossianiello97@gmail.com>
2023-05-27 16:04:46 +0200

    added index page - added button info in cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('page_title')
@@ -9,26 +8,34 @@
     <div class="container">
         <div class="row">
             @foreach ($characters as $character)
-            <div class="col-12 col-md-4 g-5">
-                <div class="card">
-                    <div class="card-head">
-                        <h3>{{$character["name"]}}</h3>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Attacco: <strong>{{$character["attack"]}}</strong></li>
-                            <li class="list-group-item">Difesa: <strong>{{$character["defence"]}}</strong></li>
-                            <li class="list-group-item">Velocità: <strong>{{$character["speed"]}}</strong></li>
-                            <li class="list-group-item">Intelligenza: <strong>{{$character["intelligence"]}}</strong></li>
-                            <li class="list-group-item">Vita: <strong>{{$character["life"]}}</strong></li>
-                            <li class="list-group-item">Descrizione: <strong> {{$character["description"]}} </strong> </li>
-                        </ul>
-                    </div>
+                <div class="col-12 col-md-4 g-5">
+                    <div class="card">
+                        <div class="card-head d-flex justify-content-between p-3">
+                            <h3>
+                                {{ $character['name'] }}
+                            </h3>
+                            <span class="rounded-3 text-center">
+                                <a href="{{ route('characters.show', ['id' => $loop->index]) }}" class="text-white">
+                                    <i class="fa-solid fa-info"></i>
+                                </a>
+                            </span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Attacco: <strong>{{ $character['attack'] }}</strong></li>
+                                <li class="list-group-item">Difesa: <strong>{{ $character['defence'] }}</strong></li>
+                                <li class="list-group-item">Velocità: <strong>{{ $character['speed'] }}</strong></li>
+                                <li class="list-group-item">Intelligenza: <strong>{{ $character['intelligence'] }}</strong>
+                                </li>
+                                <li class="list-group-item">Vita: <strong>{{ $character['life'] }}</strong></li>
+                                <li class="list-group-item">Descrizione: <strong> {{ $character['description'] }} </strong>
+                                </li>
+                            </ul>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
     </div>
-
 @endsection
