97d9533356a095f9aa4278a141228f5e536aad1f
Dan848 <daniele.ceravolo15@gmail.com>
2023-05-29 05:01:45 +0200

    fix footer css class

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,14 @@
 <footer>
-    <div class="footer mt-5 bg-dark">
+    <div class="mt-5 bg-dark">
         <div class="container d-flex align-items-center justify-content-between py-4">
-            <div><button class="text-uppercase fw-semibold btn btn-warning">sign up now!</button></div>
+            <div><button class="text-uppercase fw-semibold btn btn-warning">Play</button></div>
             <div class="d-flex align-items-center text-white">
-                <h5 class="text-uppercase dc-text-primary my-0">follow us</h5>
+                <h5 class="text-uppercase my-0">follow us</h5>
                 <div class="ms-4 fs-4">
-                    <i class="fa-brands fa-square-facebook"></i>
-                    <i class="fa-brands fa-square-twitter"></i>
-                    <i class="fa-brands fa-square-youtube"></i>
-                    <i class="fa-brands fa-square-pinterest"></i>
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-instagram"></i>
                     <i class="fa-brands fa-tiktok"></i>
                 </div>
             </div>
