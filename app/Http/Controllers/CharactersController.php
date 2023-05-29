22ca0fa900391ff94594cc45f945bf67997f0fb8
G-Lumia <giorgiolumia@gmail.com>
2023-05-29 11:13:24 +0200

    rewrote CharactersController

--- app/Http/Controllers/CharactersController.php
+++ app/Http/Controllers/CharactersController.php
@@ -2,33 +2,88 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
-use App\Models\Character;
-
 use Illuminate\Http\Request;
-
+use App\Models\Character;
 
 class CharactersController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
     public function index()
     {
         $data = config("db_partials", "dbPartials");
         $characters = Character::all();
         return view('home', compact('characters', 'data'));
     }
-    public function indexOrder($order)
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Character $character)
     {
         $data = config("db_partials", "dbPartials");
-        if($order === "name") {
-            $characters = Character::orderBy($order, "asc")->get();
-        }
-        else {
-            $characters = Character::orderBy($order, "desc")->get();
-        }
-        return view('home', compact('characters', 'data'));
+        return view('characters.show' , compact ('character' ,'data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
-
-
-
