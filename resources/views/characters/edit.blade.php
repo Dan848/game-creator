4a1d5c2b4d5bfdbbd6aa2cd246f3df1a7a30de9f
MLibralato91 <michelelibralato911@gmail.com>
2023-06-06 18:10:23 +0200

    add edit and update create

--- resources/views/characters/edit.blade.php
+++ resources/views/characters/edit.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
+
 @section('content')
     <section class="container my-4">
-        <form class="text-dark" action="{{ route('admin.characters.store') }}" method="POST">
+        <h2>Modifica il tuo personaggio</h2>
+        <form class="text-dark" action="{{ route('admin.characters.update', $character->id) }}" method="POST">
             @csrf
+            @method('PUT')
             {{-- Errors Section --}}
             @if ($errors->any())
                 <div class="alert alert-danger mt-2">
@@ -38,33 +41,34 @@
             @endif
             <div class="mb-3">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" name="name">
+                <input type="text" class="form-control" id="name" name="name" value="{{ $character->name }}">
             </div>
             <div class="mb-3">
                 <label for="attack" class="form-label">Forza</label>
-                <input type="text" class="form-control" id="attack" name="attack">
+                <input type="text" class="form-control" id="attack" name="attack" value="{{ $character->attack }}">
             </div>
             <div class="mb-3">
                 <label for="defence" class="form-label">Difesa</label>
-                <input type="text" class="form-control" id="defence" name="defence">
+                <input type="text" class="form-control" id="defence" name="defence" value="{{ $character->defence }}">
             </div>
             <div class="mb-3">
                 <label for="speed" class="form-label">Velocità</label>
-                <input type="text" class="form-control" id="speed" name="speed">
+                <input type="text" class="form-control" id="speed" name="speed" value="{{ $character->speed }}">
             </div>
             <div class="mb-3">
                 <label for="intelligence" class="form-label">Intelligenza</label>
-                <input type="text" class="form-control" id="intelligence" name="intelligence">
+                <input type="text" class="form-control" id="intelligence" name="intelligence"
+                    value="{{ $character->intelligence }}">
             </div>
             <div class="mb-3">
                 <label for="life" class="form-label">Vita</label>
-                <input type="text" class="form-control" id="life" name="life">
+                <input type="text" class="form-control" id="life" name="life" value="{{ $character->life }}">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="description" name="description" rows="4" cols="50"></textarea>
+                <textarea class="form-control" id="description" name="description" rows="4" cols="50">{{ $character->description }}</textarea>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Modifica</button>
             <button type="reset" class="btn btn-primary">Reset</button>
         </form>
     </section>
