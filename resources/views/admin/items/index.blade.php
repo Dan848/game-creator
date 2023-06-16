e1ec88bb6778cc301a09478157a6fc92e2011f88
Dan848 <daniele.ceravolo15@gmail.com>
2023-06-16 11:54:57 +0200

    fix index

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.admin')
 @section('title')
-    Personaggi
+    Oggetti
 @endsection
 
 @section('content')
     <div class="container-fluid px-4">
-        <h1 class="mt-4 text-center">Personaggi</h1>
+        <h1 class="mt-4 text-center">Oggetti</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-            <li class="breadcrumb-item active">Personaggi</li>
+            <li class="breadcrumb-item active">Oggetti</li>
         </ol>
         <div class="card text-bg-dark mb-4">
             <div class="card-header d-flex justify-content-between align-items-center">
-                <div><i class="fa-solid fa-folder-open me-1"></i>Personaggi</div>
-                <a class="btn btn-primary fw-medium d-flex align-items-center" href="{{ route('admin.characters.create') }}">
+                <div><i class="fa-solid fa-folder-open me-1"></i>Oggetti</div>
+                <a class="btn btn-primary fw-medium d-flex align-items-center" href="{{ route('admin.items.create') }}">
                     <i class="fa-regular fa-plus me-1 text-secondary fs-5 vertical-center fw-bolder"></i>Aggiungi
                 </a>
             </div>
@@ -22,46 +22,43 @@
                     <thead>
                         <tr>
                             <th scope="col">Nome</th>
-                            <th class="d-none d-sm-table-cell" scope="col">Classe</th>
-                            <th class="d-none d-lg-table-cell" scope="col">Punti Vita</th>
+                            <th class="d-none d-sm-table-cell" scope="col">Tipo</th>
+                            <th class="d-none d-lg-table-cell" scope="col">Peso</th>
                             <th class="text-center" scope="col">Azioni</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($characters as $character)
+                        @foreach ($items as $item)
                             <tr class="align-middle">
                                 {{-- Name --}}
                                 <th scope="row">
                                     <a class="h5 text-decoration-none"
-                                        href="{{ route('admin.characters.show', $character) }}">{{ $character->name }}
+                                        href="{{ route('admin.items.show', $item) }}">{{ $item->name }}
                                     </a>
                                 </th>
-                                {{-- Class/Type --}}
+                                {{-- Type --}}
                                 <td class="d-none d-sm-table-cell">
-                                    <a class="d-block img-preview">{{ $character->type->name }}</a>
+                                    <a class="d-block img-preview">{{ $item->type }}</a>
                                 </td>
                                 {{-- Life --}}
-                                <td class="d-none d-lg-table-cell">{{ $character->life }}</td>
+                                <td class="d-none d-lg-table-cell">{{ $item->weight }}</td>
                                 {{-- Action Button --}}
                                 <td>
                                     <div
                                         class="d-flex gap-2 flex-wrap justify-content-center text-center align-items-center">
 
-                                        <a class="btn btn-light"
-                                            href="{{ route('admin.characters.show', $character->slug) }}">
+                                        <a class="btn btn-light" href="{{ route('admin.items.show', $item->slug) }}">
                                             <i class="fa-solid fa-eye"></i>
                                         </a>
-                                        <a class="btn btn-secondary"
-                                            href="{{ route('admin.characters.edit', $character->slug) }}">
+                                        <a class="btn btn-secondary" href="{{ route('admin.items.edit', $item->slug) }}">
                                             <i class="fa-solid fa-pencil"></i>
                                         </a>
                                         <form class="m-0 p-0 d-inline-block"
-                                            action="{{ route('admin.characters.destroy', $character->slug) }}"
-                                            method="POST">
+                                            action="{{ route('admin.items.destroy', $item->slug) }}" method="POST">
                                             @method('DELETE')
                                             @csrf
                                             <button class="btn btn-danger delete-button"
-                                                data-item-title="{{ $character->name }}" type="submit">
+                                                data-item-title="{{ $item->name }}" type="submit">
                                                 <i class="fa-solid fa-eraser"></i>
                                             </button>
                                         </form>
@@ -71,7 +68,7 @@
                         @endforeach
                     </tbody>
                 </table>
-                {{ $characters->links('vendor.pagination.bootstrap-5') }}
+                {{ $items->links('vendor.pagination.bootstrap-5') }}
             </div>
         </div>
     </div>
--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -1,31 +1,67 @@
 @extends('layouts.admin')
 
 @section('content')
-    <section class="container my-4">
-        <form class="text-bg-dark" action="{{ route('admin.types.store') }}" method="POST">
-            @csrf
-            {{-- Errors Section --}}
-            @if ($errors->any())
-                <div class="alert alert-danger mt-2">
-                    @error('name')
-                        <p>*{{ $message }}</p>
-                    @enderror
-
-                    @error('description')
-                        <p>*{{ $message }}</p>
-                    @enderror
-                </div>
-            @endif
-            <div class="mb-3">
-                <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" name="name">
+    <div class="container mb-5">
+        <h2 class="mt-5 mb-4 text-center">
+            Nuova Classe
+        </h2>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('admin.types.index') }}">Classi</a></li>
+            <li class="breadcrumb-item active">Nuova Classe</li>
+        </ol>
+    </div>
+    <div class="container mb-4">
+        <div class="row">
+            <div class="col">
+                <form class="container form-crud" method="POST" action="{{ route('admin.characters.store') }}"
+                    enctype="multipart/form-data">
+                    @csrf
+                    {{-- Errors Section --}}
+                    @if ($errors->any())
+                        <div class="alert alert-danger mt-2">
+                            @error('name')
+                                <p>*{{ $message }}</p>
+                            @enderror
+                            @error('image')
+                                <p>*{{ $message }}</p>
+                            @enderror
+                            @error('description')
+                                <p>*{{ $message }}</p>
+                            @enderror
+                        </div>
+                    @endif
+                    <!-- NAME/IMAGE -->
+                    <div class="row">
+                        <!-- NAME -->
+                        <div class="col-12 col-md-6">
+                            <div class="form-floating mb-3">
+                                <input id="name" type="text"
+                                    class="form-control @error('name') is-invalid @enderror" name="name"
+                                    value="{{ old('name') }}" required autofocus>
+                                <label for="name">Nome</label>
+                            </div>
+                        </div>
+                        <!-- IMAGE -->
+                        <div class="col-12 col-md-6">
+                            <div class="form-floating mb-3">
+                                <input id="image" type="file"
+                                    class="form-control @error('image') is-invalid @enderror" name="image" autofocus>
+                                <label class="mb-5" for="image">Immagine</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <textarea id="description" name="description" class="form-control" id="floatingTextarea" rows="5">{{ old('description') }}</textarea>
+                        <label for="description">Descrizione</label>
+                    </div>
+                    <!-- SAVE & RESET -->
+                    <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
+                        <button class="btn btn-secondary me-2" type="reset">Reset</button>
+                        <button class="btn btn-primary ms-2" type="submit">Crea</button>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="description" name="description" rows="4" cols="50"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button type="reset" class="btn btn-primary">Reset</button>
-        </form>
-    </section>
+        </div>
+    </div>
 @endsection
