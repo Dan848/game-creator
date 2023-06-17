a97a6f276da467b430167a482b7b6e8d1057272f
Dan848 <daniele.ceravolo15@gmail.com>
2023-06-17 22:25:07 +0200

    fix show character items

--- resources/views/admin/characters/show.blade.php
+++ resources/views/admin/characters/show.blade.php
@@ -7,11 +7,26 @@
 @section('content')
     <div class="container text-white mt-5">
         <h1 class="text-center">{{ $character->name }}</h1>
-        <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('admin.characters.index') }}">Personaggi</a></li>
-            <li class="breadcrumb-item active">{{ $character->name }}</li>
-        </ol>
+        <div class="d-flex justify-content-between">
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.characters.index') }}">Personaggi</a></li>
+                <li class="breadcrumb-item active">{{ $character->name }}</li>
+            </ol>
+            <div>
+                <a class="btn btn-primary" href="{{ route('admin.characters.edit', $character->slug) }}">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+                <form class="m-0 p-0 d-inline-block" action="{{ route('admin.characters.destroy', $character->slug) }}"
+                    method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-secondary delete-button" data-item-title="{{ $character->name }}" type="submit">
+                        <i class="fa-solid fa-eraser"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
         <div class="row bg-dark rounded-5 py-5 px-4 ">
             <div class="box-image justify-content-center d-flex mb-5 mb-lg-0 col-12 col-lg-6">
                 <img src="{{ $character->image ? $character->image : $character->type->image }}"
@@ -58,11 +73,19 @@
                 </p>
                 <hr />
                 <!-- IN ATTESA CHE VENGAO SEEDATI GLI ITEM -->
-                <!-- <p class="d-flex justify-content-between">
-                                                                                                                                                                                                                                                                                                    <span class="pixel-text"> Arma</span>
-                                                                                                                                                                                                                                                                                                    <span class="fw-bold"> $character->items->name </span>
-                                                                                                                                                                                                                                                                                                  </p>
-                                                                                                                                                                                                                                                                                                  <hr /> -->
+                <div>
+                    <p class="text-center">Inventario:</p>
+                    @foreach ($character->items as $item)
+                        @if (!$loop->last)
+                            <p>
+                                <span class="fst-italic">{{ $item->name }} - </span>
+                            @else
+                                <span class="fst-italic">{{ $item->name }}</span>
+                            </p>
+                        @endif
+                    @endforeach
+                </div>
+                <hr />
             </div>
             <p class="p-3 d-flex justify-content-between col-12 flex-column">
                 <span class="text-secondary text-center fs-4 py-3 pixel-text">Descrizione</span>
