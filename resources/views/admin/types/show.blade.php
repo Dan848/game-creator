652f1b42cd990b279415c2e03b71c5262a0b82a9
G-Lumia <giorgiolumia@gmail.com>
2023-06-18 13:45:26 +0200

    fixed description of type show

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -5,31 +5,41 @@
 @endsection
 
 @section('content')
-    <div class="container d-flex my-5 justify-content-center">
-        <div class="info me-5">
-            <h1 class="pb-3">{{ $type['name'] }}</h1>
-            <ul class="text-black list-unstyled">
+<div class="container text-white mt-5">
+    <h1 class="text-center">{{ $type->name }}</h1>
+        <div class="d-flex justify-content-between">
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.characters.index') }}">Classi</a></li>
+                <li class="breadcrumb-item active">{{ $type->name }}</li>
+            </ol>
+            <div>
+                <a class="btn btn-primary" href="{{ route('admin.characters.edit', $type->slug) }}">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+                <form class="m-0 p-0 d-inline-block" action="{{ route('admin.characters.destroy', $type->slug) }}"
+                    method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-secondary delete-button" data-item-title="{{ $type->name }}" type="submit">
+                        <i class="fa-solid fa-eraser"></i>
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div class="row bg-dark justify-content-center rounded-5 py-5 px-4 ">
+            <div class="box-image d-flex  align-items-center d-flex mb-5 mb-lg-0 col-12 col-lg-6">
+                <img src="{{ $type->image }}" alt="{{ $type->name }}" class="object-fit-contain w-75" />
+            </div>
+           <ul class="text-light list-unstyled">
+                @php
+                    $formattedText = preg_replace('/###\s*(.*)/', '<br><span class="feat">$1</span><br>', $type['description']);
+                    $formattedAgain = str_replace('*', '<br>', $formattedText);
+                @endphp
                 <li>
-                    Descrizione: <span class="fw-bold">{{ $type['description'] }}</span>
+                    <span>{!! $formattedAgain !!}</span>
                 </li>
             </ul>
-
-            <form action="{{ route('admin.types.destroy', $type) }}" method="POST">
-
-                @csrf
-                @method('DELETE')
-                <button class="delete-button btn-warning btn"><a href="{{ route('admin.types.edit', $type->id) }}"><i
-                            class="fa-solid fa-pencil"></i></a>
-                </button>
-                <button type='submit' class="delete-button btn-danger btn" data-item-title="{{ $type->name }}"> <i
-                        class="fa-solid  fa-trash"></i></button>
-
-            </form>
-        </div>
-        <div class="image-character-show">
-
-            <img src="https://cdn.vox-cdn.com/thumbor/TMjYvNH-wm9yE2CcRbhL6b0Sv8w=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22701491/boh.jpg"
-                alt="{{ $type['name'] }}" class="img-fluid">
         </div>
     </div>
 @endsection
