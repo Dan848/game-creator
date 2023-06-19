6492307d08b78c7edcd6e9ade531af2b7578b409
MLibralato91 <michelelibralato911@gmail.com>
2023-06-19 15:25:06 +0200

    fix edit item value

--- resources/views/admin/items/edit.blade.php
+++ resources/views/admin/items/edit.blade.php
@@ -106,7 +106,7 @@
                             <div class="form-floating mb-3">
                                 <input id="dice_num" type="number"
                                     class="form-control @error('dice_num') is-invalid @enderror" name="dice_num"
-                                    value="{{ old('dice_num') }}" autofocus>
+                                    value="{{ $item->dice_num }}" autofocus>
                                 <label for="dice_num">Numero di Dadi</label>
                             </div>
                         </div>
@@ -114,8 +114,8 @@
                         <div class="col-12 col-md-6">
                             <div class="form-floating mb-3">
                                 <input id="dice_faces" name="dice_faces" type="number"
-                                    class="form-control @error('dice_faces') is-invalid @enderror" value="{{ old('dice_faces') }}"
-                                    autofocus>
+                                    class="form-control @error('dice_faces') is-invalid @enderror"
+                                    value="{{ $item->dice_faces }}" autofocus>
                                 <label class="mb-5" for="dice_faces">Numero di facce</label>
                             </div>
                         </div>
